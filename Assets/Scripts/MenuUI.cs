6b6b00f1f0eb4e09e7667927d6a5aecf3fa41b4a
Cringeek <119015446+Cringeek@users.noreply.github.com>
2022-11-26 04:31:51 +0300

    commit
    - Best score text in main menu fixed

--- Assets/Scripts/MenuUI.cs
+++ Assets/Scripts/MenuUI.cs
@@ -8,8 +8,16 @@ using UnityEngine.UI;
 public class MenuUI : MonoBehaviour
 {
     [SerializeField] private Text nameText;
+    [SerializeField] private Text bestScoreText;
     public static string currentPlayerName;
-   
+
+
+    public void Start()
+    {
+        DataSave.LoadHighScore();
+        bestScoreText.text = $"Best Score: {DataSave.playerName} : {DataSave.score}";
+    }
+
     public void StartButton()
     {     
         currentPlayerName = nameText.text;
